dfa51ab5bb4b6d1063bd75e6d9ad2ec458a729b2
Laura Barauna <laura.barauna@senior.com.br>
2026-05-25 15:09:30 -0300

    [FEAT][DADOS-004]: Cria fila de prioridade.

---
+++ src/fila/FilaPrioridade.java
@@ -0,0 +1,34 @@
+package fila;
+
+public class FilaPrioridade<T> implements Fila<T> {
+    private ListaFila<T> lista;
+
+    FilaPrioridade() {
+        this.lista = new ListaFila<>();
+    }
+
+    @Override
+    public void inserir(T valor) {
+        this.lista.inserir(valor);
+    }
+
+    @Override
+    public T peek() {
+        return this.lista.peek();
+    }
+
+    @Override
+    public T retirar() {
+        return this.lista.retirar();
+    }
+
+    @Override
+    public void liberar() {
+        this.lista.liberar();
+    }
+
+    @Override
+    public boolean estaVazia() {
+        return this.lista.estaVazia();
+    }
+}
