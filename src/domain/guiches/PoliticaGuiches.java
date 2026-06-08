3678abb564037ac89c5da596e743e156fa54a57b
Laura Barauna <laura.barauna@senior.com.br>
2026-06-08 13:22:08 -0300

    [REFACTOR][DADOS-016]: Retira imports não usados.

--- src/domain/guiches/PoliticaGuiches.java
+++ src/domain/guiches/PoliticaGuiches.java
@@ -1,8 +1,6 @@
 package domain.guiches;
 
-import domain.entities.RegistroAtendimento;
 import infrastructure.structures.fila.Fila;
-import infrastructure.structures.fila.FilaDinamica;
 
 public interface PoliticaGuiches {
     void chamarProximo(Fila filaGeral, Fila filaPreferencial);
