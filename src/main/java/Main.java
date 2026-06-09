abb29ec82c7dfa4798728346c84544829fa0138b
Laura Barauna <laura.barauna@senior.com.br>
2026-06-09 14:33:05 -0300

    [FEAT][DADOS-012]: Move classes.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,46 @@
 import controller.GuicheController;
+import controller.RelatorioController;
 import domain.entities.Guiche;
 import domain.entities.RegistroAtendimento;
 import domain.guiches.GuicheGeral;
 import domain.guiches.GuichePreferencial;
 import domain.structures.EstruturaDados;
+import infrastructure.report.PdfReport;
 import infrastructure.structures.fila.FilaDinamica;
 import infrastructure.structures.pilha.PilhaDinamica;
+import presentation.views.TelaPrincipal;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+
         EstruturaDados<RegistroAtendimento> filaGeral = new FilaDinamica<>();
         EstruturaDados<RegistroAtendimento> filaPreferencial = new FilaDinamica<>();
 
-        EstruturaDados<RegistroAtendimento> historicoGuicheGeral = new PilhaDinamica<>();
-        EstruturaDados<RegistroAtendimento> historicoGuichePreferencial = new PilhaDinamica<>();
+        EstruturaDados<RegistroAtendimento> pilhaGeral = new PilhaDinamica<>();
+        EstruturaDados<RegistroAtendimento> pilhaPreferencial = new PilhaDinamica<>();
 
-        Guiche guicheNormal = new Guiche(filaGeral, filaPreferencial, historicoGuicheGeral, new GuicheGeral());
-        Guiche guichePreferencial = new Guiche(filaGeral, filaPreferencial, historicoGuichePreferencial, new GuichePreferencial());
+        Guiche guicheNormal = new Guiche(filaGeral, filaGeral, filaPreferencial, new GuicheGeral());
+        Guiche guichePreferencial = new Guiche(filaGeral, filaPreferencial, pilhaPreferencial, new GuichePreferencial());
 
-        GuicheController controller = new GuicheController();
-        controller.atenderClienteFilaGeral(guicheNormal);
-        controller.atenderClienteFilaPreferencial(guichePreferencial);
+        GuicheController guicheController = new GuicheController(guicheNormal, guichePreferencial);
+        RelatorioController relatorioController = new RelatorioController(guicheNormal, guichePreferencial, new PdfReport());
+
+        SwingUtilities.invokeLater(() -> {
+
+            JFrame frame = new JFrame("Gerenciamento de Fila Bancário");
+
+            TelaPrincipal tela = new TelaPrincipal(guicheController);
+
+            frame.setContentPane(tela.getPanel());
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+
+        });
     }
+
 }
