0f9b495439c0085341a3eee973bcd2440a0e2468
Laura Barauna <laura.barauna@senior.com.br>
2026-06-09 14:16:59 -0300

    [FEAT][DADOS-018]: Renomeia interface.

--- src/main/java/domain/report/Report.java
+++
@@ -1,9 +0,0 @@
-package domain.report;
-
-import domain.entities.RegistroAtendimento;
-import domain.structures.EstruturaDados;
-import domain.structures.EstruturaVetor;
-
-public interface Report {
-    void gerarRelatorio(EstruturaVetor<RegistroAtendimento> pilhaGeral, EstruturaVetor<RegistroAtendimento> pilhaPreferencial);
-}
