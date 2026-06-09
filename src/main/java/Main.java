ccd9ccaffde8230e10ef2ab1234bf92a44e773a8
Laura Barauna <laura.barauna@senior.com.br>
2026-06-09 13:34:17 -0300

    [FEAT][DADOS-008]: (LAURA) Alterando main para não dar erro.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,18 @@ import domain.guiches.GuicheGeral;
 import domain.guiches.GuichePreferencial;
 import domain.structures.EstruturaDados;
 import infrastructure.structures.fila.FilaDinamica;
+import infrastructure.structures.pilha.PilhaDinamica;
 
 public class Main {
     public static void main(String[] args) {
         EstruturaDados<RegistroAtendimento> filaGeral = new FilaDinamica<>();
         EstruturaDados<RegistroAtendimento> filaPreferencial = new FilaDinamica<>();
 
+        EstruturaDados<RegistroAtendimento> historicoGuicheGeral = new PilhaDinamica<>();
+        EstruturaDados<RegistroAtendimento> historicoGuichePreferencial = new PilhaDinamica<>();
 
-        Guiche guicheNormal = new Guiche(filaGeral, filaPreferencial, new GuicheGeral());
-        Guiche guichePreferencial = new Guiche(filaGeral, filaPreferencial, new GuichePreferencial());
+        Guiche guicheNormal = new Guiche(filaGeral, filaPreferencial, historicoGuicheGeral, new GuicheGeral());
+        Guiche guichePreferencial = new Guiche(filaGeral, filaPreferencial, historicoGuichePreferencial, new GuichePreferencial());
 
         GuicheController controller = new GuicheController();
         controller.atenderClienteFilaGeral(guicheNormal);
