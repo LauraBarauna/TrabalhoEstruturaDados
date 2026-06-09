0b6df400e96c761266341cdd762d60c0faa7c3fc
Laura Barauna <laura.barauna@senior.com.br>
2026-06-09 16:29:52 -0300

    [FEAT][DADOS-014]: Adiciona método para incluir cliente na fila.

--- src/main/java/application/usecase/AdicionarClienteUC.java
+++ src/main/java/application/usecase/AdicionarClienteUC.java
@@ -1,12 +1,20 @@
 package application.usecase;
 
 
+import domain.entities.Pessoa;
 import domain.entities.RegistroAtendimento;
+import domain.structures.EstruturaDados;
 
 public class AdicionarClienteUC {
 
-    public void execute(RegistroAtendimento registroAtendimento) {
-        registroAtendimento.adicionarCliente();
+    public void execute(Pessoa pessoa, EstruturaDados<RegistroAtendimento> filaGeral, EstruturaDados<RegistroAtendimento> filaPreferencial) {
+        RegistroAtendimento registroAtendimento = new RegistroAtendimento(pessoa);
+
+        if (pessoa.isPrioritario()) {
+            filaPreferencial.inserir(registroAtendimento);
+        } else {
+            filaGeral.inserir(registroAtendimento);
+        }
     }
 
 }
--- src/main/java/controller/GuicheController.java
+++ src/main/java/controller/GuicheController.java
@@ -1,17 +1,24 @@
 package controller;
 
 
+import application.usecase.AdicionarClienteUC;
 import application.usecase.AtenderClienteUC;
 import domain.entities.Guiche;
+import domain.entities.Pessoa;
+import domain.entities.RegistroAtendimento;
+import domain.structures.EstruturaDados;
 
 public class GuicheController {
 
     private final AtenderClienteUC atenderCliente;
+    private final AdicionarClienteUC  adicionarCliente;
+
     private final Guiche guicheNormal;
     private final Guiche guichePreferencial;
 
     public GuicheController(Guiche guicheNormal, Guiche guichePreferencial) {
         this.atenderCliente = new AtenderClienteUC();
+        this.adicionarCliente = new AdicionarClienteUC();
         this.guicheNormal = guicheNormal;
         this.guichePreferencial = guichePreferencial;
     }
@@ -22,6 +29,14 @@ public class GuicheController {
 
     public void atenderClientePreferencial() {
         this.atenderCliente.execute(this.guichePreferencial);
+    }
+
+    public void adicionarClienteNaFila(Pessoa pessoa, EstruturaDados<RegistroAtendimento> filaGeral, EstruturaDados<RegistroAtendimento> filaPreferencial) {
+        try {
+            this.adicionarCliente.execute(pessoa, filaGeral, filaPreferencial);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 }
