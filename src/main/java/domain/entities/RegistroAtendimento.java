a9afc182d6a42d63aa1d496dc3a065ac8ea4d900
Aikadevil <yasmin_ms@hotmail.com>
2026-06-09 13:41:22 -0300

    [FEAT][DADOS-006]: adicionado Pessoa em classe RegistroAtendimento

--- src/main/java/domain/entities/RegistroAtendimento.java
+++ src/main/java/domain/entities/RegistroAtendimento.java
@@ -7,16 +7,17 @@ import infrastructure.structures.fila.FilaDinamica;
 import java.time.LocalTime;
 
 public class RegistroAtendimento {
+
+    private Pessoa pessoa;
     private LocalTime horarioEntrada;
     private LocalTime horarioInicio;
     private long tempoAtendimentoMin;
 
-    // TODO adicionar cliente
-
     FilaDinamica<RegistroAtendimento> filaGeral;
     FilaDinamica<RegistroAtendimento> filaPreferencial;
 
-    public RegistroAtendimento(FilaDinamica<RegistroAtendimento> filaGeral, FilaDinamica<RegistroAtendimento> filaPreferencial) {
+    public RegistroAtendimento(Pessoa pessoa,FilaDinamica<RegistroAtendimento> filaGeral, FilaDinamica<RegistroAtendimento> filaPreferencial) {
+        this.pessoa = pessoa;
         this.filaGeral = filaGeral;
         this.filaPreferencial = filaPreferencial;
     }
@@ -24,7 +25,11 @@ public class RegistroAtendimento {
     public void adicionarCliente() {
         this.horarioEntrada = LocalTime.now();
 
-        // TODO adicionar lógica do cliente (se for maior que 60 anos adicionar na fila preferencial)
+        if (pessoa.isPrioritario()) {
+            filaPreferencial.inserir(this);
+        } else {
+            filaGeral.inserir(this);
+        }
         this.filaPreferencial.inserir(this);
         this.filaGeral.inserir(this);
     }
@@ -40,6 +45,8 @@ public class RegistroAtendimento {
     public long getTempoAtendimentoMin() {
         return tempoAtendimentoMin;
     }
+
+    public Pessoa getPessoa() { return pessoa; }
 
     public void setHorarioEntrada(LocalTime horarioEntrada) {
         this.horarioEntrada = horarioEntrada;
